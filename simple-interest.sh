291596df61f5dec6c77fac6e647db81441176c21
Jose <130668791+jjuradotx@users.noreply.github.com>
2025-03-09 11:29:22 +0300

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,7 +1,6 @@
    #!/bin/bash
    # This script calculates simple interest given principal,
    # annual rate of interest and time period in years.
-   # Do not use this in production. Sample purpose only.
    # Input:
    # p, principal amount
    # t, time period in years
